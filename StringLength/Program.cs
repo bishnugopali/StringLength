7bf1e8fd54a2036c3fec371abc8746719f72b9f8
Bishnu Gopali <bishnugopali@hotmail.com>
2018-02-11 10:50:20 +0000

    StringLength

--- StringLength/Program.cs
+++ StringLength/Program.cs
@@ -10,6 +10,22 @@ namespace StringLength
     {
         static void Main(string[] args)
         {
+            string str; /* Declares a string of size 100 */
+            int l = 0;
+
+            Console.Write("\n\nFind the length of a string :\n");
+            Console.Write("---------------------------------\n");
+            Console.Write("Input the string : ");
+            str = Console.ReadLine();
+
+            foreach (char ch in str)
+            {
+                l += 1;
+            }
+
+            Console.Write("Length of the string is : {0}\n\n", l);
+
+            Console.ReadLine();
         }
     }
 }
